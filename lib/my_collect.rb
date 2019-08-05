3344a510ae6faaf7db750c976d4a6be08733cd25
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 15:56:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(argument)
+  counter = 0
+  collection = []
+  while counter < argument.length do
+      collection << yield(argument[counter])
+      counter += 1
+    end
+    collection
+end
